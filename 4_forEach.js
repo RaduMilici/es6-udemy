b21591c160913ebcd4f924adfb2d8487a126bf92
Radu Milici <moguay@gmail.com>
2016-09-05 16:12:45 +0000

    forEach

---
+++ 4_forEach.js
@@ -0,0 +1,10 @@
+var colors = ['red', 'blue', 'green'];
+
+// es5
+for(var i = 0; i < colors.length; i++)
+  console.log(colors[i]);
+
+// es6
+colors.forEach(function(color){
+  console.log(color);
+});
